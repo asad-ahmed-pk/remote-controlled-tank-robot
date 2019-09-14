5c8ff56d5f696ec2f69d3a4ad71d0162465a3f5e
Asad Ahmed <asad.ahmed.de@gmail.com>
2019-09-14 21:27:55 +0200

    Updated bluetooth scan test

--- ros_ws/src/bluetooth_controller_teleop/src/bluetooth/bluetooth_test.cpp
+++ ros_ws/src/bluetooth_controller_teleop/src/bluetooth/bluetooth_test.cpp
@@ -11,6 +11,8 @@ int main(int argc, char** argv)
 {
     std::vector<std::tuple<std::string, std::string>> devices;
 
+    std::cout << "\nScanning for devices" << std::endl;
+
     BluetoothManager manager;
     if (manager.ScanDevices(devices))
     {
@@ -18,9 +20,13 @@ int main(int argc, char** argv)
             std::cout << "\nAddress: " << std::get<0>(device);
             std::cout << " Name: " << std::get<1>(device) << std::endl;
         }
+
+        if (devices.empty()) {
+            std::cout << "\nCould not find any devices in range" << std::endl;
+        }
     }
     else {
-        std::cout << "Failed to connected to bluetooth adapter" << std::endl;
+        std::cout << "Failed to connect to bluetooth adapter" << std::endl;
     }
 
     return 0;
